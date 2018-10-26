2cabe6ad202292ef6ecfc365ec757ba21c8cee07
tomcs <devs@getbraintree.com>
2018-10-26 11:20:40 -0700

    4-6 - add cmds for handling variables

--- ch4/4.3/calculator.c
+++ ch4/4.3/calculator.c
@@ -5,19 +5,28 @@
 
 #define MAXOP    100  /* max size of operand or operator */
 #define NUMBER   '0'  /* signal that a number was found */
+#define VARIABLE '1'  /* signal that a variable was found */
 #define MAXVAL   100  /* max size of val stack */
 #define BUFSIZE  100  /* character input buffer */
+#define NVARS    26   /* number of variables */
 
-int sp = 0;           /* next free stack position */
-double val[MAXVAL];   /* value stack */
-char buf[BUFSIZE];    /* buffer for ungetch */
-int bufp = 0;         /* pointer to next free pos in buf */
+int sp = 0;                   /* next free stack position */
+double val[MAXVAL];           /* value stack */
+double vars[NVARS];           /* variable values */
+double varsetflags[NVARS];    /* variable set? */
+char buf[BUFSIZE];            /* buffer for ungetch */
+int bufp = 0;                 /* pointer to next free pos in buf */
+char lastprintvar = 'z';      /* var holding value of last print */
 
 int getch(void);
 void ungetch(int);
 int getop(char []);
 void push(double);
 double pop(void);
+void setvar(char, double);
+double getvar(char);
+void clearvars(void);
+int isset(char);
 
 /* reverse polish notation */
 int main()
@@ -31,6 +40,14 @@ int main()
     switch (type) {
       case NUMBER:
         push(atof(s));
+        break;
+      case VARIABLE:
+        if (isset(s[0])) {
+          push(getvar(s[0]));
+        }
+        else {
+          push(s[0]);
+        }
         break;
       case '+':
         push(pop() + pop());
@@ -55,6 +72,13 @@ int main()
           push((int) pop() % (int) op2);
         else
           printf("error: zero divisor\n");
+        break;
+      case '=': /* variable assignment */
+        op2 = pop();
+        if ((op1 = pop()) == lastprintvar)
+          printf("error: %c is a reserved variable\n", lastprintvar);
+        else
+          setvar(op1, op2);
         break;
       case '~': /* sin */
         push(sin(pop()));
@@ -93,7 +117,9 @@ int main()
         val[sp] = 0.0;
         break;
       case '\n':
-        printf("\t%.8g\n", pop());
+        clearvars();
+        setvar(lastprintvar, pop());
+        printf("\t%.8g\n", getvar(lastprintvar));
         break;
       default:
         printf("error: unknown command %s\n", s);
@@ -131,6 +157,10 @@ int getop(char s[])
   while ((s[0] = c = getch()) == ' ' || c == '\t')
     ;
   s[1] = '\0';
+  if (c >= 'a' && c <= 'z') {
+    s[0] = c;
+    return VARIABLE;
+  }
   if (!isdigit(c) && c != '.' && c != '-')
     return c;
 
@@ -170,4 +200,28 @@ void ungetch(int c)
     printf("ungetch: too many characters\n");
   else
     buf[bufp++] = c;
+}
+
+void setvar(char var, double val)
+{
+  vars[var - 'a'] = val;
+  varsetflags[var - 'a'] = 1;
+}
+
+double getvar(char var)
+{
+  return vars[var - 'a'];
+}
+
+int isset(char var)
+{
+  return varsetflags[var - 'a'];
+}
+
+void clearvars(void)
+{
+  for (int i=0; i<NVARS; i++) {
+    vars[i] = 0;
+    varsetflags[i] = 0;
+  }
 }
