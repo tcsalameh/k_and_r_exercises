479b4b4075fa4cf8b845e366f7315ec808a23f37
Tom Christiansen-Salameh <tomsalameh@gmail.com>
2018-11-10 21:19:34 -0800

    5-8

---
+++ ch5/5.7/days.c
@@ -0,0 +1,66 @@
+/* There is no error checking in day_of_year or month_day.
+ * Remedy this defect */
+#include <stdio.h>
+
+static char daytab[2][13] = {
+  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(int year, int month, int day);
+int month_day(int year, int yearday, int *pmonth, int *pday);
+
+int main()
+{
+  int err, month, day;
+
+  printf("day_of_year(2016, 3, 16) = %d, expected %d\n", day_of_year(2016, 3, 16), 76);
+  printf("day_of_year(2018, 3, 16) = %d, expected %d\n", day_of_year(2018, 3, 16), 75);
+  printf("day_of_year(2018, 13, 16) = %d, expected %d\n", day_of_year(2018, 13, 16), -1);
+  printf("day_of_year(2018, 2, 29) = %d, expected %d\n", day_of_year(2018, 2, 29), -2);
+
+  err = month_day(2016, 76, &month, &day);
+  printf("month_day(2016, 75, m, d) = %d %d, expected %d %d\n", month, day, 3, 16);
+  err = month_day(2018, 76, &month, &day);
+  printf("month_day(2018, 75, m, d) = %d %d, expected %d %d\n", month, day, 3, 17);
+  err = month_day(2018, 366, &month, &day);
+  printf("month_day(2018, 366, m, d) = %d, expected %d\n", err, -2);
+  err = month_day(2018, 365, &month, &day);
+  printf("month_day(2018, 365, m, d) = %d %d, expected %d %d\n", month, day, 12, 31);
+  err = month_day(2016, 365, &month, &day);
+  printf("month_day(2018, 365, m, d) = %d, expected %d\n", err, -2);
+}
+
+/* day_of_year: set day of year from month and day */
+int day_of_year(int year, int month, int day)
+{
+  if (month < 1 || month > 12 || year < 0)
+    return -1;
+
+  int i, leap;
+
+  leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+  if (day > daytab[leap][month])
+    return -2;
+  for (i = 1; i < month; i++)
+    day += daytab[leap][i];
+  return day;
+}
+
+/* month_day: set month, day from day of year */
+int month_day(int year, int yearday, int *pmonth, int *pday)
+{
+  int i, leap;
+
+  if (year < 0)
+    return -1;
+
+  leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+  if ((leap && (yearday > 364)) || yearday > 365)
+    return -2;
+  for (i = 1; yearday > daytab[leap][i]; i++)
+    yearday -= daytab[leap][i];
+  *pmonth = i;
+  *pday = yearday;
+  return 0;
+}
