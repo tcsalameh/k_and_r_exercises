5976a45a799275490c49546d804e3838b6beebaa
Tom Christiansen-Salameh <tomsalameh@gmail.com>
2018-11-12 21:52:19 -0800

    5.10

---
+++ ch5/5.10/echo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* echo command-line args: 1st version */
+int main(int argc, char *argv[])
+{
+  int i;
+
+  for (i = 1; i < argc; i++)
+    printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
+  printf("\n");
+  return 0;
+}
