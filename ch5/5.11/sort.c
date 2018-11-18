2bc397e0de419b7a121c64d3495a9f8b9a27e25a
tomcs <devs@getbraintree.com>
2018-11-18 10:22:32 -0800

    5-17

--- ch5/5.11/sort.c
+++ ch5/5.11/sort.c
@@ -7,6 +7,9 @@
 /* 5-16: Add the -d (directory order) option, which makes
  * comparisons only on letters, numbers, and blanks. Make sure it
  * works in conjunction with -f. */
+/* 5-17: Add a field handling capability, so sorting may be done
+ * on fields within lines, each field sorted according to an
+ * independent set of options. */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -18,12 +21,15 @@ char *lineptr[MAXLINES]; /* pointers to text lines */
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
 
-void q_sort(void *lineptr[], int left, int right, int reverse,
-           int fold, int directory, int (*comp) (void *, void *));
+void q_sort(void *lineptr[], int left, int right,
+            int reverse, int fold, int directory,
+            int fieldnum, char fieldsep,
+            int (*comp) (void *, void *));
 int numcmp(char *, char *);
-int parseargs(int argc, char *argv[], int parsedargs[]);
+int parseargs(int argc, char *argv[], char parsedargs[]);
 void lower(char *);
 void dirformat(char *, char *);
+int cut(char *, int, char);
 
 /* sort input lines */
 int main (int argc, char *argv[])
@@ -32,13 +38,17 @@ int main (int argc, char *argv[])
   int numeric;        /* whether to sort by numeric value */
   int reverse;        /* sort in reverse order */
   int fold;           /* fold upper and lower case chars together */
-  int dir;           /* directory order, ignore non-alphanumeric chars */
+  int dir;            /* directory order, ignore non-alphanumeric chars */
+  int fieldnum;       /* field number to sort on */
+  char sep;           /* seperator for fields */
 
-  int args[] = {
-    0, // numeric
-    1, // reverse
-    0, // fold
-    0  // dir
+  char args[] = {
+    0,   // numeric
+    1,   // reverse
+    0,   // fold
+    0,   // dir
+    -1,  // fieldnum
+    ' ', // seperator
   };
   if (!parseargs(argc, argv, args)) {
     printf("Error parsing arguments\n");
@@ -49,9 +59,12 @@ int main (int argc, char *argv[])
   reverse = args[1];
   fold = args[2];
   dir = args[3];
+  fieldnum = args[4];
+  sep = args[5];
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    q_sort((void **) lineptr, 0, nlines - 1, reverse, fold, dir,
-        (int (*)(void*,void*)) (numeric ? numcmp : strcmp));
+    q_sort((void **) lineptr, 0, nlines - 1,
+           reverse, fold, dir, fieldnum, sep,
+           (int (*)(void*,void*)) (numeric ? numcmp : strcmp));
     writelines(lineptr, nlines);
     return 0;
   } else {
@@ -60,17 +73,31 @@ int main (int argc, char *argv[])
   }
 }
 
-int parseargs(int argc, char *argv[], int parsedargs[])
+int parseargs(int argc, char *argv[], char parsedargs[])
 {
   while (--argc > 0 && ++argv) {
     if (strcmp(*argv, "-n") == 0)
-        parsedargs[0] = 1;
+      // numeric sort
+      parsedargs[0] = 1;
     else if (strcmp(*argv, "-r") == 0)
-        parsedargs[1] = -1;
+      // reverse order
+      parsedargs[1] = -1;
     else if (strcmp(*argv, "-f") == 0)
-        parsedargs[2] = 1;
+      // treat upper/lower case the same
+      parsedargs[2] = 1;
     else if (strcmp(*argv, "-d") == 0)
-        parsedargs[3] = 1;
+      // directory order
+      parsedargs[3] = 1;
+    else if (strcmp(*argv, "-F") == 0) {
+      // field number to sort on
+      parsedargs[4] = atof(*(++argv));
+      argc--;
+    }
+    else if (strcmp(*argv, "-s") == 0) {
+      // field separator
+      parsedargs[5] = *(++argv)[0];
+      argc--;
+    }
     else
       return 0;
   }
@@ -80,8 +107,10 @@ int parseargs(int argc, char *argv[], int parsedargs[])
 
 
 /* qsort: sort v[left]...v[right] into increasing order */
-void q_sort(void *v[], int left, int right, int reverse, int fold, int directory,
-           int (*comp)(void *, void *))
+void q_sort(void *v[], int left, int right,
+            int reverse, int fold, int directory,
+            int fieldnum, char fieldsep,
+            int (*comp)(void *, void *))
 {
   int i, last;
   void swap(void *v[], int, int);
@@ -91,13 +120,31 @@ void q_sort(void *v[], int left, int right, int reverse, int fold, int directory
   swap(v, left, (left + right) / 2);
   last = left;
   for (i = left+1; i<= right; i++) {
-    if (fold || directory) {
-      char rcopy[strlen(v[i])], lcopy[strlen(v[left])];
+    if (fold || directory || fieldnum >= 0) {
+      char l[strlen(v[i])], r[strlen(v[left])];
+      char *rcopy = l;
+      char *lcopy = r;
       directory ? dirformat(rcopy, v[i]) : strcpy(rcopy, v[i]);
       directory ? dirformat(lcopy, v[left]) : strcpy(lcopy, v[left]);
       if (fold) {
         lower(rcopy);
         lower(lcopy);
+      }
+      if (fieldnum >= 0) {
+        int rstart = cut(rcopy, fieldnum, fieldsep);
+        int lstart = cut(lcopy, fieldnum, fieldsep);
+        if (rstart >= 0)
+          rcopy += rstart;
+        else {
+          printf("ERROR: String '%s' has fewer than %d fields separated by '%c'.\n", v[i], fieldnum, fieldsep);
+          return;
+        }
+        if (lstart >= 0)
+          lcopy += lstart;
+        else {
+          printf("ERROR: String '%s' has fewer than %d fields separated by '%c'.\n", v[left], fieldnum, fieldsep);
+          return;
+        }
       }
       if ((*comp)(rcopy, lcopy) * reverse < 0)
         swap(v, ++last, i);
@@ -107,8 +154,34 @@ void q_sort(void *v[], int left, int right, int reverse, int fold, int directory
     }
   }
   swap(v, left, last);
-  q_sort(v, left, last-1, reverse, fold, directory, comp);
-  q_sort(v, last+1, right, reverse, fold, directory, comp);
+  q_sort(v, left, last-1, reverse, fold, directory, fieldnum, fieldsep, comp);
+  q_sort(v, last+1, right, reverse, fold, directory, fieldnum, fieldsep, comp);
+}
+
+/* return how far forward the field begins
+ * relative to pointer
+ * and set the end of the field to '\0' */
+int cut(char *str, int fieldnum, char sep)
+{
+  int fieldcount = 0;
+  int i = 0;
+
+  while (str[i] != '\0') {
+    if (fieldcount == fieldnum) {
+      int j = i;
+      for (; str[j] != '\0' && str[j] != sep; j++)
+        ;
+      str[j] = '\0';
+      return i;
+    }
+    if (str[i] == sep) {
+      fieldcount++;
+      while (str[++i] == sep)
+        ;
+    } else
+      i++;
+  }
+  return -1;
 }
 
 /* replaces str with lowercase version */
