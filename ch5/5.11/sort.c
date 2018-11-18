2b5674125e5d35c60d8802843a829ebfa8658163
tomcs <devs@getbraintree.com>
2018-11-17 18:13:49 -0800

    problem 5-14

--- ch5/5.11/sort.c
+++ ch5/5.11/sort.c
@@ -1,3 +1,6 @@
+/* 5-14: Modify the sort program to handle a -r flag, which
+ * indicates sorting in reverse (decreasing) order. Be sure
+ * that -r works with -n. */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,20 +12,31 @@ char *lineptr[MAXLINES]; /* pointers to text lines */
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
 
-void q_sort(void *lineptr[], int left, int right,
+void q_sort(void *lineptr[], int left, int right, int reverse,
            int (*comp) (void *, void *));
 int numcmp(char *, char *);
+int parseargs(int argc, char *argv[], int parsedargs[]);
 
 /* sort input lines */
 int main (int argc, char *argv[])
 {
   int nlines;         /* number of input lines read */
-  int numeric = 0;    /* pointers to text lines */
+  int numeric;        /* whether to sort by numeric value */
+  int reverse;        /* sort in reverse order */
 
-  if (argc > 1 && strcmp(argv[1], "-n") == 0)
-    numeric = 1;
+  int args[] = {
+    0, // numeric
+    1  // reverse
+  };
+  if (!parseargs(argc, argv, args)) {
+    printf("Error parsing arguments\n");
+    return 1;
+  }
+
+  numeric = args[0];
+  reverse = args[1];
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    q_sort((void **) lineptr, 0, nlines -1,
+    q_sort((void **) lineptr, 0, nlines -1, reverse,
         (int (*)(void*,void*)) (numeric ? numcmp : strcmp));
     writelines(lineptr, nlines);
     return 0;
@@ -32,8 +46,23 @@ int main (int argc, char *argv[])
   }
 }
 
+int parseargs(int argc, char *argv[], int parsedargs[])
+{
+  while (--argc > 0 && ++argv) {
+    if (strcmp(*argv, "-n") == 0)
+        parsedargs[0] = 1;
+    else if (strcmp(*argv, "-r") == 0)
+        parsedargs[1] = -1;
+    else
+      return 0;
+  }
+
+  return 1;
+}
+
+
 /* qsort: sort v[left]...v[right] into increasing order */
-void q_sort(void *v[], int left, int right,
+void q_sort(void *v[], int left, int right, int reverse,
            int (*comp)(void *, void *))
 {
   int i, last;
@@ -44,11 +73,11 @@ void q_sort(void *v[], int left, int right,
   swap(v, left, (left + right) / 2);
   last = left;
   for (i = left+1; i<= right; i++)
-    if ((*comp)(v[i], v[left]) < 0)
+    if ((*comp)(v[i], v[left]) * reverse < 0)
       swap(v, ++last, i);
   swap(v, left, last);
-  q_sort(v, left, last-1, comp);
-  q_sort(v, last+1, right, comp);
+  q_sort(v, left, last-1, reverse, comp);
+  q_sort(v, last+1, right, reverse, comp);
 }
 
 /* numcpm: compare s1 and s2 numerically */
