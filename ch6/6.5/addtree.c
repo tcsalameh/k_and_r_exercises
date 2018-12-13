00a94f39923638f556a6a505a93f21d974812a65
tomcs <devs@getbraintree.com>
2018-12-12 19:48:39 -0800

    ch 6.5 wip

---
+++ ch6/6.5/addtree.c
@@ -0,0 +1,31 @@
+struct tnode *talloc(void);
+char *strdup(char *);
+
+/* addtree: add a node with w, at or below p */
+struct tnode *addtree(struct tnode *p, char *w)
+{
+  int cond;
+
+  if (p == NULL) {
+    p = talloc();
+    p->word = strdup(w);
+    p->count = 1;
+    p->left = p->right = NULL;
+  } else if ((cond = strcmp(w, p->word)) == 0)
+    p->count++;
+  else if (cond < 0)
+    p->left = addtree(p->left, w);
+  else
+    p->right = addtree(p->right, w);
+  return p;
+}
+
+/* in order traversal */
+void treeprint(struct tnode *p)
+{
+  if (p != NULL) {
+    treeprint(p->left);
+    printf("%4d %s\n", p->count, p->word);
+    treeprint(p->right);
+  }
+}
